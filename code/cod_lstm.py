d7c98b4833865eb23bf0cb6c3cbe07ac5eb659ba
vlopes <vinicius.lopes@cea.com.br>
2019-01-10 16:23:14 -0200

    preprocessing lstm initiated

--- code/cod_lstm.py
+++ code/cod_lstm.py
@@ -1,8 +1,26 @@
 import os
 import pandas as pd
 import pickle
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
+os.getcwd()
+os.chdir("../../")
 
-infile = open(,'rb')
-new_dict = pickle.load(infile)
+#Initial cleaning (encapsulate)
+infile = open("./github_vavlopes/data/df2014-06-15.pickle",'rb')
+df = pickle.load(infile)
 infile.close()
+df.head(2)
+#acertando os casos que vieram corrompidos em dir_vento
+df = df.where(a > -20000)
+df = df.dropna(0)
+X = df.filter(regex = 'x|y|z').values
+
+#Function for data standardizing
+X = X.astype(str)
+Enc = LabelEncoder()
+X[:,0] = Enc.fit_transform(X[:,0])
+X[:,1] = Enc.fit_transform(X[:,1])
+X[:,2] = Enc.fit_transform(X[:,2])
+OneHot = OneHotEncoder(categorical_features = [0,1,2])
+X = OneHot.fit_transform(X).toarray()

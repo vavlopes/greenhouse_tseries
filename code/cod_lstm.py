5849c9b2ad5cadca25c33440dfc40e4640edfd8f
vavlopes <v094546@gmail.com>
2019-01-24 01:13:25 +0000

    working but not scaling

--- code/cod_lstm.py
+++ code/cod_lstm.py
@@ -1,10 +1,20 @@
+
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+
 import os
 import pandas as pd
 import pickle
+import random
 import numpy as np
-from matplotlib import pyplot
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
-from sklearn.model_selection import GroupKFold, cross_val_score
+#from matplotlib import pyplot
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.model_selection import GroupKFold, cross_val_score, cross_validate, ParameterGrid, GridSearchCV
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.pipeline import Pipeline
 import itertools
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.models import Sequential
@@ -103,14 +113,11 @@ df = df.loc[df.concat_coord == concat_coord[0],:]
 df = df.drop(['concat_coord'], axis = 1)
 train, test = Holdout_split(df)
 X_train, X_test, y_train, y_test = scaleNum_col(train, test)
-#X_train = create_indexcol(X_train)
 #print(pd.DataFrame(X_train))
-
-n_features = int(X_train.shape[1]/(7-hmlook_back))
-
 
 def reshape_data(X_train, X_test, y_train, y_test):
 
+    n_features = int(X_train.shape[1]/(7-hmlook_back))
     X_train = X_train.reshape(X_train.shape[0],(7-hmlook_back),n_features)
     X_test = X_test.reshape(X_test.shape[0],(7-hmlook_back),n_features)
     y_train = y_train.reshape(y_train.shape[0],)
@@ -118,54 +125,113 @@ def reshape_data(X_train, X_test, y_train, y_test):
 
     return(X_train, X_test, y_train, y_test)
 
-
 X_train, X_test, y_train, y_test = reshape_data(X_train, X_test, y_train, y_test)
-a,b = X_train.shape[1], X_train.shape[2]
-def modeling_LSTM(a,b):
-    #global a,b
+time_step, n_features = X_train.shape[1], X_train.shape[2]
+def modeling_LSTM():
     model = Sequential()
-    model.add(LSTM(100, kernel_regularizer=regularizers.l2(0.01), input_shape=(a,b))) #input_shape = (time_step, number of features)
+    model.add(LSTM(100, kernel_regularizer=regularizers.l2(0.01)))#, input_shape=(time_step, n_features))) #input_shape = (time_step, number of features)
     model.add(Dense(1,activity_regularizer=regularizers.l1(0.01)))
-    model.compile(loss='mse', optimizer='adam', metrics=['mae']) #alterei para adpatar para classificacao
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
     return model
 
-estimator = KerasRegressor(build_fn = modeling_LSTM, a = a, b = b, epochs = 10, batch_size = 2, verbose = 0)
+estimator = KerasRegressor(build_fn = modeling_LSTM, time_step = time_step, n_features = n_features, epochs = 10, batch_size = 2, verbose = 2)
+
+#transformer = MinMaxScaler()
+#regr = TransformedTargetRegressor(regressor = estimator, transformer = transformer)
 indexes = create_indexcol(X_train)
 group_kfold = GroupKFold(n_splits=5)
-#y_train = y_train.reshape(y_train.shape[0],)
 group_kfold = group_kfold.split(X_train, y_train, groups = indexes)
 gkf = list(group_kfold)
-results = cross_val_score(estimator, X_train, y_train, cv = gkf)
+grid = [{'epochs': [10,20], 'batch_size': [5,10]}]
+grid = ParameterGrid(grid)
+resultados = []
+for i in range(len(list(grid))):
+    results = cross_val_score(estimator, X_train, y_train, fit_params = list(grid)[i], cv = gkf, scoring='neg_mean_absolute_error')
+    resultados_part = (np.mean(results),list(grid)[i]['batch_size'],list(grid)[i]['epochs'])
+    resultados.append(resultados_part)
 
-results
+#svc = make_pipeline(StandardScaler(), svm.SVC(C=1.0, kernel="linear"))
+#a = cross_validate(svc, x, y, cv = list(group_kfold), scoring='f1')
+a = pd.DataFrame(resultados,columns=['score', 'batch_size', 'epochs'])
+
+b = a.sort_values('score', ascending  = True).iloc[1,:]
+print(b)
+
+# group_kfold = GroupKFold(n_splits=5)
+# group_kfold = group_kfold.split(X_train, y_train, groups = indexes)
+# cvscores = []
+# for train, test in kfold.split(X, Y):
+#   # create model
+#     model = Sequential()
+#     model.add(LSTM(100, kernel_regularizer=regularizers.l2(0.01)))#, input_shape=(time_step, n_features))) #input_shape = (time_step, number of features)
+#     model.add(Dense(1,activity_regularizer=regularizers.l1(0.01)))
+#     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+#
+#
+#     # Fit the model
+# 	model.fit(X[train], Y[train], epochs=150, batch_size=10, verbose=0)
+# 	# evaluate the model
+# 	scores = model.evaluate(X[test], Y[test], verbose=0)
+# 	print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# 	cvscores.append(scores[1] * 100)
 
 
-gkf
-X_train.shape
-y_train.shape
-#group_kfold = group_kfold.split(X_train, y_train, groups = indexes)
+
+
+
+#one possible solution
+# def drop_nans(X, y=None):
+#     total = X.shape[1]
+#     new_thresh = total - thresh
+#     df = pd.DataFrame(X)
+#     df.dropna(thresh=new_thresh, inplace=True)
+#     return df.values
+
+#transformer = FunctionTransformer(drop_nans, validate=False)
+
+
+
+
+
+#
+# import tensorflow as tf
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+#
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
+#
+# from keras import backend as K
+# K.tensorflow_backend._get_available_gpus()
+#
+# import keras
+# config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 4} )
+# sess = tf.Session(config=config)
+# keras.backend.set_session(sess)
 
 
 
 
 
 
-    # fit network
-    # it could be good to use a batch size equal to the number of registers per "matricula" (maybe use the mean)
-    history = model.fit(X_train, y_train, epochs=1000, batch_size=1, validation_data=(X_test, y_test), verbose=2, shuffle=False)
-    # plot history
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
-    pyplot.legend()
-    pyplot.show()
 
-    yhat = model.predict(X_test)
-    return(yhat)
+#array([-0.07965646, -0.07173461, -0.06853584, -0.05444147, -0.07200295])
 
-
-
-X_train, X_test, y_train, y_test = reshape_data(X_train, X_test, y_train, y_test)
-yhat = modeling_LSTM(X_train, X_test, y_train, y_test)
+#     # fit network
+#     # it could be good to use a batch size equal to the number of registers per "matricula" (maybe use the mean)
+#     history = model.fit(X_train, y_train, epochs=1000, batch_size=1, validation_data=(X_test, y_test), verbose=2, shuffle=False)
+#     # plot history
+#     pyplot.plot(history.history['loss'], label='train')
+#     pyplot.plot(history.history['val_loss'], label='test')
+#     pyplot.legend()
+#     pyplot.show()
+#
+#     yhat = model.predict(X_test)
+#     return(yhat)
+#
+#
+#
+# X_train, X_test, y_train, y_test = reshape_data(X_train, X_test, y_train, y_test)
+# yhat = modeling_LSTM(X_train, X_test, y_train, y_test)
 
 
 #def mounting_results(yhat):

20218d22e09945c7e98d72ada01f7ea074f5b8ee
vavlopes <v094546@gmail.com>
2019-02-17 15:35:33 +0000

    Results viz updated, mae progress ok and ypred x yobs implemented

--- code/results_viz.R
+++ code/results_viz.R
@@ -3,25 +3,27 @@ library(tidyverse)
 rm(list = ls())
 gc()
 
-# getting BRT results in same format
-files_to_c = list.files("../../results/brt", pattern = "ypred", recursive = T, full.names = T)
+# Getting all results in the same shape ----------------------------------------------
+
+files_to_c = list.files("../../results/svm", pattern = "ypred", recursive = T, full.names = T)
 for(i in 1:length(files_to_c)){
-  res = read.table(files_to_viz[[i]],header = T, sep = " ", skip = 0)
-  if(unique(res$cenario) == "Cenario_9"){
-    res  = res %>% mutate(tecnica = "brt") %>% rename(yobs = truth, ypred = response)
-    res = res %>% select(-c(iter,set,id))
-    res = res %>% mutate(range_datas = range_cen9, data = data_cen9, hora = hora_cen9,
-                         concat_coord = concat_cen9)
-    res = res[,c("tecnica","cenario","range_datas","concat_coord","data",
-                 "hora","target","hmlook_back","yobs","ypred")]
-    write.table(res, file = files_to_viz[[i]] )
-    
+  res = read.table(files_to_c[[i]],header = T, sep = " ", skip = 0)
+  if(unique(res$cenario) == "Cenario_1"){
+    res = res %>% mutate(concat_coord = concat_cen1)
+    write.table(res, file = files_to_c[[i]] )
+
   }
 }
 
 
+files_tbmodel = list.files('../../data/',
+                           full.names = T,
+                           pattern = ".RData")
 
-
+dat = get(load(files_tbmodel[[1]]))
+test_lines = which(dat$data %in% unique(dat$data)[8:10])
+ver = dat[test_lines,] %>% arrange(data,hora)
+concat_cen1 = ver$concat_coord
 
 
 
@@ -33,11 +35,37 @@ for(i in 1:length(files_to_viz)){
   res[[i]] = read.table(files_to_viz[[i]],header = T, sep = " ", skip = 0) %>% 
     mutate_if(is.factor, as.character)
 }  
-results = do.call(bind_rows, res) %>% mutate(erro = ypred - yobs)
+results = do.call(bind_rows, res) %>% mutate(erro = ypred - yobs) %>% 
+  mutate(abs_error = abs(erro))
 
 
 # Plotting MAE progress over Scenarios -----------------------------------
-res1 = results %>% group_by(tecnica,target,cenario, hmlook_back) %>% summarise(mae = mean(abs(erro)))
+res1 = results %>% group_by(tecnica,target,cenario, hmlook_back) %>% 
+  summarise(mae = mean(abs_error)) %>% as.data.frame()
 res1 %>% filter(target == "temp") %>% 
   ggplot(aes(x = hmlook_back, y = mae, col = cenario)) + geom_line()+
-  facet_wrap(.~tecnica)
+  facet_wrap(.~tecnica, scales  = "free")
+
+# Plotting ypred x yobs in test set ---------------------------------------
+
+#Fits ok for the variability_part1
+res2 = results %>% 
+  filter(target == "temp",
+         hmlook_back == 1, tecnica == "brt") %>% 
+  arrange(data,hora) %>% group_by(concat_coord) %>% 
+  mutate(timestamp = seq(1:length(yobs))) %>%
+  gather(tipo_medida, medida, c(yobs,ypred)) %>%
+  as.data.frame()
+
+res2 %>% ggplot(aes(x = timestamp, y = medida, col = tipo_medida)) + geom_line() + 
+  facet_wrap(~cenario, ncol = 2, scales = "free") + xlab('Timestamp') + 
+  ylab('Temperatura (°C)') +
+  scale_color_manual(labels = c("Real", "Predito"),values=c(rgb(1,0,0), rgb(0,0,1,0.7))) +
+  theme_bw() + theme(text = element_text(size = 7)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  theme(axis.text.x = element_text(size = 6))+
+  theme(axis.text.y = element_text(size=6))+
+  theme(axis.title = element_text(size=12))+
+  theme(legend.text=element_text(size=10))+
+  theme(legend.title = element_text(size = 10))+
+  theme(strip.text = element_text(size = 10))
